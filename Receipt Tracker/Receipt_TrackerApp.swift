47a0f37375534148bab3f120443710f6770b48e3
bogstebole <bogstedsgn@gmail.com>
2025-12-15 14:46:20 +0100

    Initial Commit

---
+++ Receipt Tracker/Receipt_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Receipt_TrackerApp.swift
+//  Receipt Tracker
+//
+//  Created by Bogdan Stefanovic on 15. 12. 2025..
+//
+
+import SwiftUI
+
+@main
+struct Receipt_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
